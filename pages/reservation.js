2fa917f1753e7052eb2b71639c0216ca77963f96
williamolyslager <wolyslager@abnormalsecurity.com>
2022-06-12 15:38:32 -0500

    nit '

--- pages/reservation.js
+++ pages/reservation.js
@@ -2,8 +2,6 @@ import { reservationMachine } from "../utils/state-machine";
 import { useMachine } from "@xstate/react";
 
 const reservation = () => {
-  const [current, send] = useMachine(reservationMachine);
-
   return null;
 };
 
