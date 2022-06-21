cb248c8bdbbab460259b88d386f1950b84a394ec
williamolyslager <wolyslager@abnormalsecurity.com>
2022-06-20 20:24:23 -0500

    Creating card component

--- components/states/InitialState.jsx
+++ components/states/InitialState.jsx
@@ -3,6 +3,15 @@ import { useMachine } from "@xstate/react";
 import { reservationMachine } from "../../utils/state-machine";
 import Image from "next/image";
 
+const CardComponent = (props) => {
+  return (
+    <div className="py-6 px-12 bg-white flex flex-col rounded-xl shadow-lg border-2 border-black">
+      {props.icon}
+      <span className="text-xl font-semibold">{props.text}</span>
+    </div>
+  );
+};
+
 const InitialState = () => {
   const [state, send] = useMachine(reservationMachine);
   return (
@@ -11,25 +20,30 @@ const InitialState = () => {
         <div className="space-between space-y-8">
           <span className="text-5xl">Select Activity</span>
         </div>
+
         <div className="flex space-x-16">
-          <div className="py-6 px-12 bg-white flex flex-col rounded-xl shadow-lg border-2 border-black">
-            <Image
-              src="/icons/scuba-diver.svg"
-              alt=""
-              width={150}
-              height={150}
-            />
-            <span className="text-xl font-semibold">Recreational Dive</span>
-          </div>
-          <div className="py-6 px-12 bg-white rounded-xl flex flex-col shadow-lg border-2 border-black">
-            <Image
-              src="/icons/certificate.svg"
-              alt=""
-              width={150}
-              height={150}
-            />
-            <span className="text-xl font-semibold">Certification Dive</span>
-          </div>
+          <CardComponent
+            icon={
+              <Image
+                src="/icons/scuba-diver.svg"
+                alt=""
+                width={150}
+                height={150}
+              />
+            }
+            text="Recreational Dive"
+          />
+          <CardComponent
+            icon={
+              <Image
+                src="/icons/certificate.svg"
+                alt=""
+                width={150}
+                height={150}
+              />
+            }
+            text="Certification Dive"
+          />
         </div>
       </div>
     </div>
