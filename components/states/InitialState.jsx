4616c7ffd887d11e7c330d7fbb5a467ead969a71
williamolyslager <wolyslager@abnormalsecurity.com>
2022-06-20 20:47:51 -0500

    hover and onclick logic for card component

--- components/states/InitialState.jsx
+++ components/states/InitialState.jsx
@@ -5,7 +5,10 @@ import Image from "next/image";
 
 const CardComponent = (props) => {
   return (
-    <div className="py-6 px-12 bg-white flex flex-col rounded-xl shadow-lg border-2 border-black">
+    <div
+      className="py-6 px-12 bg-white flex flex-col hover:bg-sky-600 hover:scale-105 cursor-pointer rounded-xl shadow-lg border-2 border-black"
+      onClick={props.onClick}
+    >
       {props.icon}
       <span className="text-xl font-semibold">{props.text}</span>
     </div>
@@ -32,6 +35,7 @@ const InitialState = () => {
               />
             }
             text="Recreational Dive"
+            onClick={() => console.log("testing")}
           />
           <CardComponent
             icon={
@@ -43,6 +47,7 @@ const InitialState = () => {
               />
             }
             text="Certification Dive"
+            onClick={() => console.log("testing")}
           />
         </div>
       </div>
