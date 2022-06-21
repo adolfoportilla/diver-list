600ff827b04d2adc4012a8ee8372df515ffbeba5
williamolyslager <wolyslager@abnormalsecurity.com>
2022-06-20 20:22:34 -0500

    creating card component

--- components/states/InitialState.jsx
+++ components/states/InitialState.jsx
@@ -3,15 +3,48 @@ import { useMachine } from "@xstate/react";
 import { reservationMachine } from "../../utils/state-machine";
 import Image from "next/image";
 
+const CardComponent = (props) => {
+  return (
+    <div className="py-6 px-12 bg-white flex flex-col rounded-xl shadow-lg border-2 border-black">
+      {props.icon}
+      <span className="text-xl font-semibold">{props.text}</span>
+    </div>
+  );
+};
+
 const InitialState = () => {
   const [state, send] = useMachine(reservationMachine);
   return (
-    <div>
-      <div>
-        <Image src="/icons/scuba-diver.svg" alt="" width={100} height={100} />
-      </div>
-      <div>
-        <Image src="/icons/certificate.svg" alt="" width={100} height={100} />
+    <div className="h-screen flex items-center justify-center">
+      <div className="flex flex-col items-center space-y-16 mb-80">
+        <div className="space-between space-y-8">
+          <span className="text-5xl">Select Activity</span>
+        </div>
+
+        <div className="flex space-x-16">
+          <CardComponent
+            icon={
+              <Image
+                src="/icons/scuba-diver.svg"
+                alt=""
+                width={150}
+                height={150}
+              />
+            }
+            text="Recreational Dive"
+          />
+          <CardComponent
+            icon={
+              <Image
+                src="/icons/certificate.svg"
+                alt=""
+                width={150}
+                height={150}
+              />
+            }
+            text="Certification Dive"
+          />
+        </div>
       </div>
     </div>
   );
