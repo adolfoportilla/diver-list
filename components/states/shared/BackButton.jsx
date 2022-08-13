ec5b196d4cabcb350d364700655c8b6066d8c0d7
williamolyslager <wolyslager@abnormalsecurity.com>
2022-08-13 15:24:39 -0500

    back button

---
+++ components/states/shared/BackButton.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { ArrowBack } from "@mui/icons-material";
+
+const BackButton = (props) => {
+  return (
+    <div className="cursor-pointer">
+      <ArrowBack fontSize="large" />
+    </div>
+  );
+};
+
+export default BackButton;
