887e52dd8181471e6bc50c5bf892d40e2a819b29
williamolyslager <wolyslager@abnormalsecurity.com>
2022-08-14 12:29:36 -0500

    commenting out backbtn

--- components/states/shared/StatePage.jsx
+++ components/states/shared/StatePage.jsx
@@ -5,7 +5,7 @@ const StatePage = (props) => {
     <div className="flex justify-center">
       <div className="flex flex-col items-center mt-12 mx-5 md:mt-32 space-y-8 md:space-y-12 w-full md:w-3/5">
         <div className="w-full">
-          <BackButton labelStyle={{ fontSize: "200%" }} />
+          {/* <BackButton labelStyle={{ fontSize: "200%" }} /> */}
         </div>
         {props.children}
       </div>
