844b0ee41d6ac841448be95c1c4dbded1569cf55
Adolfo Portilla <aportilla@abnormalsecurity.com>
2022-08-15 21:11:34 -0500

    Feat: add percel env variable

--- utils/supabase.js
+++ utils/supabase.js
@@ -2,7 +2,10 @@ import { createClient } from "@supabase/supabase-js";
 
 let client = {};
 
-if (process.env.NODE_ENV === "production") {
+if (
+  process.env.NODE_ENV === "production" ||
+  ["production", "preview"].includes(process.env.REACT_APP_VERCEL_ENV)
+) {
   const supabaseUrl = "https://jssciplbktcfzvelizfx.supabase.co";
   const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 
