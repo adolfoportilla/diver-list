50e53a0cdfd857bbf71873d0098d834ab33e2da7
Adolfo Portilla <aportilla@abnormalsecurity.com>
2022-08-15 21:26:53 -0500

    test new settings

--- utils/supabase.js
+++ utils/supabase.js
@@ -2,14 +2,18 @@ import { createClient } from "@supabase/supabase-js";
 
 let client = {};
 
-if (
-  process.env.NODE_ENV === "production" ||
-  ["production", "preview"].includes(process.env.REACT_APP_VERCEL_ENV)
-) {
-  const supabaseUrl = "https://jssciplbktcfzvelizfx.supabase.co";
-  const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
+// if (
+//   process.env.NODE_ENV === "production" ||
+//   ["production", "preview"].includes(process.env.REACT_APP_VERCEL_ENV)
+// ) {
+//   const supabaseUrl = "https://jssciplbktcfzvelizfx.supabase.co";
+//   const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
 
-  client = createClient(supabaseUrl, supabaseKey);
-}
+//   client = createClient(supabaseUrl, supabaseKey);
+// }
+export default createClient(
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
+);
 
-export default client;
+// export default client;
