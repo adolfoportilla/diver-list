6d8be86eccf5df226be572923462b0787d25cd47
Adolfo Portilla <aportilla@abnormalsecurity.com>
2022-08-15 21:50:42 -0500

    fix: nit

--- utils/supabase.js
+++ utils/supabase.js
@@ -1,19 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
-let client = {};
-
-// if (
-//   process.env.NODE_ENV === "production" ||
-//   ["production", "preview"].includes(process.env.REACT_APP_VERCEL_ENV)
-// ) {
-//   const supabaseUrl = "https://jssciplbktcfzvelizfx.supabase.co";
-//   const supabaseKey = process.env.REACT_APP_SUPABASE_KEY;
-
-//   client = createClient(supabaseUrl, supabaseKey);
-// }
 export default createClient(
   process.env.NEXT_PUBLIC_SUPABASE_URL,
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
-
-// export default client;
