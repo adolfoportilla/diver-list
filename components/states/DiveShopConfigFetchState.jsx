ef18447345d30a1834b3269f80e684b0312bfcf7
williamolyslager <wolyslager@abnormalsecurity.com>
2022-09-04 13:39:51 -0500

    nit

--- components/states/DiveShopConfigFetchState.jsx
+++ components/states/DiveShopConfigFetchState.jsx
@@ -20,10 +20,6 @@ const DiveShopConfigFetchState = () => {
     if (url_hash) {
       fetchData(url_hash);
     }
-    send({
-      type: STATE_ACTIONS.FETCH_SUCCESS,
-      value: "",
-    });
   }, [url_hash]);
 
   const fetchData = async function (url_hash) {
