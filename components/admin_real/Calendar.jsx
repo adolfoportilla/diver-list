5cdc4f31c0e4fde741342db9d4e74fd625dc0b7d
Adolfo Portilla <adolfoportilla@users.noreply.github.com>
2022-10-02 16:30:15 -0500

    Feat: add calendar nits (#49)
    * Feat: add calendar nits
    
    * Fix: nits 2
    
    Co-authored-by: Adolfo Portilla <aportilla@abnormalsecurity.com>

--- components/admin_real/Calendar.jsx
+++ components/admin_real/Calendar.jsx
@@ -3,7 +3,7 @@ import "antd/dist/antd.css";
 import { Badge, Calendar as _Calendar } from "antd";
 import moment from "moment";
 
-import { fetchCalendar } from "../../utils/supabase";
+import supabase from "../../utils/supabase";
 
 const isSameDay = (dateA, dateB) => {
   return (
@@ -49,20 +49,70 @@ const dateCellRender = (cellDate, data) => {
     </ul>
   );
 };
+const PAGE_SIZE = 20;
+
+async function fetchCalendar(dateFilter = null) {
+  // TODO:
+  // We need to fetch only the reservations from that store.
+  const baseQueryset = supabase
+    .from("reservations")
+    .select("*")
+    .order("date", { ascending: false })
+    .order("time", { ascending: true });
+  // TODO:
+  // We need to paginate, because if not the request is going to get pretty expensive.
+  // .limit(PAGE_SIZE);
+
+  // TODO: limit reservations based on date.
+  // if (dateFilter) {
+  // baseQueryset.gte("date", dateFilter);
+  // }
+  const { data, error } = await baseQueryset;
+
+  return { data, error };
+}
+
+const formattedTodayDay = (date) => {
+  let [month, day, year] = [
+    date.getMonth(),
+    date.getDate(),
+    date.getFullYear(),
+  ];
+  month += 1;
+
+  if (month < 10) {
+    month = `0${month}`;
+  }
+
+  return `${year}-${month}-01`;
+};
+
 // https://ant.design/components/calendar/
 export default function Calendar() {
   const [reservations, setReservations] = React.useState();
   React.useEffect(() => {
     (async () => {
       const { data, error } = await fetchCalendar();
+      // formattedTodayDay(new Date())
       setReservations(data);
     })();
   }, []);
 
   return (
+    // TODO
+    // The biggest problem with the current approach is we fetch everything in one call, which will
+    // cause issues if there are hundreds of reservations.
+    // Ideally, we only fetch the reservations of the current month, and when a user changes the
+    // month, we fetch more
     <_Calendar
       dateCellRender={(cellDate) => dateCellRender(cellDate, reservations)}
       monthCellRender={null}
+      onPanelChange={(date) => {
+        // fetchCalendar(formattedTodayDay(date.toDate())).then((v) => {
+        //   const { data, error } = v;
+        //   setReservations(data);
+        // });
+      }}
     />
   );
 }
--- utils/supabase.js
+++ utils/supabase.js
@@ -5,17 +5,4 @@ const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
-const PAGE_SIZE = 20;
-
-export async function fetchCalendar() {
-  const { data, error } = await supabase
-    .from("reservations")
-    .select("*")
-    .order("date", { ascending: false })
-    .order("time", { ascending: true })
-    .limit(PAGE_SIZE);
-
-  return { data, error };
-}
-
 export default supabase;
