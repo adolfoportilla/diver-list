2fc1519ed6a6e448b4e3b046a6a58e22658b3910
Adolfo Portilla <aportilla@abnormalsecurity.com>
2022-10-02 11:57:15 -0500

    Fix: nits 2

--- utils/supabase.js
+++ utils/supabase.js
@@ -5,19 +5,4 @@ const supabase = createClient(
   process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
-const PAGE_SIZE = 20;
-
-export async function fetchCalendar() {
-  const { data, error } = await supabase
-    .from("reservations")
-    .select("id, date, time, diver_certified")
-    .order("date", { ascending: false })
-    .order("time", { ascending: true })
-    .gte("date", "2022-09-28")
-    .eq("diver_certified", true)
-    .limit(PAGE_SIZE);
-
-  return { data, error };
-}
-
 export default supabase;
