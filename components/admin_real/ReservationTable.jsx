21464148b2cdbcd073d8999e3a670238e36bacc4
Adolfo Portilla <adolfoportilla@users.noreply.github.com>
2022-10-03 19:39:22 -0500

    Feat: reservations pagination (#53)
    Co-authored-by: Adolfo Portilla <aportilla@abnormalsecurity.com>

--- components/admin_real/ReservationTable.jsx
+++ components/admin_real/ReservationTable.jsx
@@ -3,83 +3,33 @@ import { DataGrid } from "@mui/x-data-grid";
 
 import supabase from "../../utils/supabase";
 
-const VALUE_GETTER_FN = (params) => {
-  if (params.field.includes(".")) {
-    const [outer, inner] = params.field.split(".");
-    return params.row[outer][inner];
-  }
-  return params.value;
-};
+import { PAGE_SIZE, TABLE_COLUMNS, calculateRange } from "./utils";
 
-const DEFAULT_SORT = { field: "date", ascending: false };
-
-const PAGE_SIZE = 10;
-
-const DEFAULT_ROW_PROPS = {
-  sortable: false,
-  hideable: false,
-  filterable: false,
-  valueGetter: VALUE_GETTER_FN,
-};
-
-// https://mui.com/x/api/data-grid/grid-col-def/
-const columns = [
-  { field: "date", headerName: "Date" },
-  { field: "time", headerName: "Time", sortable: false, width: 80 },
-  {
-    field: "reservation_type",
-    headerName: "Reservation Type",
-    sortable: false,
-    width: 135,
-  },
-  {
-    field: "diver_certified",
-    headerName: "Certified?",
-    width: 90,
-  },
-  {
-    field: "diver_information.name",
-    headerName: "Name",
-  },
-  {
-    field: "diver_information.lastName",
-    headerName: "Last Name",
-  },
-  {
-    field: "diver_information.age",
-    headerName: "Age",
-    type: "number",
-    width: 50,
-  },
-  { field: "number_of_dives", headerName: "Experience", width: 120 },
-  {
-    field: "diver_information.email",
-    headerName: "Email",
-    minWidth: 200,
-  },
-].map((value) => ({ ...DEFAULT_ROW_PROPS, ...value }));
-
-const parseData = (data = []) => {
-  if (!data) {
-    return [];
-  }
-  return data;
+const fetchReservations = async (props) => {
+  return await supabase
+    .from("reservations")
+    .select("*", { count: "exact" })
+    .order("date", { ascending: false })
+    .range(props.rangeInitial, props.rangeEnd);
 };
 
 export default function ReservationTable() {
   const [data, setData] = React.useState([]);
   const [isLoading, setIsLoading] = React.useState(false);
 
+  const [count, setCount] = React.useState(0);
+
   React.useEffect(() => {
     (async () => {
       setIsLoading(true);
-      const { data: reservations, error } = await supabase
-        .from("reservations")
-        .select("*")
-        .order("date", { ascending: false })
-        .limit(PAGE_SIZE);
+      const {
+        data: reservations,
+        error,
+        count: rowCount,
+      } = await fetchReservations({ rangeInitial: 0, rangeEnd: PAGE_SIZE - 1 });
       setData(reservations);
       setIsLoading(false);
+      setCount(rowCount);
     })();
   }, []);
 
@@ -87,14 +37,9 @@ export default function ReservationTable() {
     <div className="bg-white">
       {/* https://mui.com/x/api/data-grid/data-grid/ */}
       <DataGrid
-        rows={parseData(data)}
-        columns={columns}
+        rows={data}
+        columns={TABLE_COLUMNS}
         pageSize={PAGE_SIZE}
-        initialState={{
-          pagination: {
-            pageSize: PAGE_SIZE,
-          },
-        }}
         rowsPerPageOptions={[PAGE_SIZE]}
         isRowSelectable={() => false}
         loading={isLoading}
@@ -102,6 +47,20 @@ export default function ReservationTable() {
         filterOperators={[]}
         disableColumnMenu
         autoHeight
+        rowCount={count}
+        pagination
+        paginationMode="server"
+        onPageChange={(newPage) => {
+          const [initial, end] = calculateRange(newPage, PAGE_SIZE);
+          setIsLoading(true);
+          fetchReservations({ rangeInitial: initial, rangeEnd: end }).then(
+            ({ data, error, count }) => {
+              setData(data);
+              setIsLoading(false);
+              setCount(count);
+            }
+          );
+        }}
       />
     </div>
   );
---
+++ components/admin_real/utils.js
@@ -0,0 +1,59 @@
+// This function allows the columns to get values from Supabase like this `userInformation.name`
+// it essentially works the same way as the `get` function from lodash. (https://lodash.com/docs/4.17.15#get)
+const valueGetterFn = (params) => {
+  if (params.field.includes(".")) {
+    const [outer, inner] = params.field.split(".");
+    return params.row[outer][inner];
+  }
+  return params.value;
+};
+
+export const PAGE_SIZE = 10;
+
+export const DEFAULT_ROW_PROPS = {
+  sortable: false,
+  hideable: false,
+  filterable: false,
+  valueGetter: valueGetterFn,
+};
+
+// https://mui.com/x/api/data-grid/grid-col-def/
+export const TABLE_COLUMNS = [
+  { field: "date", headerName: "Date" },
+  { field: "time", headerName: "Time", sortable: false, width: 80 },
+  {
+    field: "reservation_type",
+    headerName: "Reservation Type",
+    sortable: false,
+    width: 135,
+  },
+  {
+    field: "diver_certified",
+    headerName: "Certified?",
+    width: 90,
+  },
+  {
+    field: "diver_information.name",
+    headerName: "Name",
+  },
+  {
+    field: "diver_information.lastName",
+    headerName: "Last Name",
+  },
+  {
+    field: "diver_information.age",
+    headerName: "Age",
+    type: "number",
+    width: 50,
+  },
+  { field: "number_of_dives", headerName: "Experience", width: 120 },
+  {
+    field: "diver_information.email",
+    headerName: "Email",
+    minWidth: 200,
+  },
+].map((value) => ({ ...DEFAULT_ROW_PROPS, ...value }));
+
+export const calculateRange = (currentPage, pageSize) => {
+  return [currentPage * pageSize, (currentPage + 1) * pageSize - 1];
+};
