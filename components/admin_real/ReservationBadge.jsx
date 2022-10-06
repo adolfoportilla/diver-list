7e1e74f821e16ce46b598313f8378bd7acab110c
William Olyslager <wolyslager@abnormalsecurity.com>
2022-10-05 19:42:34 -0500

    adding data to modal (#59)

--- components/admin_real/ReservationBadge.jsx
+++ components/admin_real/ReservationBadge.jsx
@@ -5,13 +5,13 @@ import { Badge } from "antd";
 
 const ReservationBadge = ({ item }) => {
   const [modalOpen, setModalOpen] = useState(false);
+
   return (
     <>
       <Badge
-        status={item.type}
+        status="processing"
         text={
           <>
-            <span className="">{item.content}</span>
             <span className="ml-1 font-bold text-sm">{item.time}</span>
           </>
         }
@@ -23,7 +23,16 @@ const ReservationBadge = ({ item }) => {
         visible={modalOpen}
         onOk={() => setModalOpen(false)}
         onCancel={() => setModalOpen(false)}
-      ></Modal>
+        footer={null}
+      >
+        <div className="flex flex-col">
+          <span>{`Name: ${item.diver_information.name} ${item.diver_information.lastName}`}</span>
+          <span>{`Reservation Type: ${item.reservation_type}`}</span>
+          <span>{`Diver Experience: ${item.number_of_dives}`}</span>
+          <span>{`Date: ${item.date}`}</span>
+          <span>{`Time: ${item.time}`}</span>
+        </div>
+      </Modal>
     </>
   );
 };
