0c8ae441ef8a19b1a5f4828aa6e8210a11195af8
williamolyslager <wolyslager@abnormalsecurity.com>
2022-10-11 22:16:12 -0500

    additional functionality for crud +bug fixes

--- components/admin_real/desktop/EditReservationButton.jsx
+++ components/admin_real/desktop/EditReservationButton.jsx
@@ -2,7 +2,7 @@ import React, { useState } from "react";
 import { Edit } from "@mui/icons-material";
 import { Button } from "@mui/material";
 import Modal from "antd/lib/modal/Modal";
-import { DatePicker, Input } from "antd";
+import { Alert, DatePicker, Input } from "antd";
 import { TimePicker } from "antd";
 import moment from "moment";
 import { Select } from "antd";
@@ -34,13 +34,30 @@ const EditReservationButton = ({ reservation }) => {
 
   //todo fix null bug on timepicker
 
+  const reservationValid = () => {
+    if (
+      date &&
+      time &&
+      certified &&
+      reservationType &&
+      experience &&
+      name &&
+      lastName &&
+      age &&
+      email
+    ) {
+      return true;
+    }
+    return false;
+  };
+
   return (
     <div>
       <Modal
         title={"Edit Reservation"}
         visible={modalOpen}
         onOk={() => {
-          if (date && time && certified && reservationType && experience) {
+          if (reservationValid()) {
             const { data, error } = updateReservation({
               date,
               time,
@@ -48,6 +65,12 @@ const EditReservationButton = ({ reservation }) => {
               reservationType,
               experience,
               id,
+              diverInformation: {
+                name: name,
+                lastName: lastName,
+                age: age,
+                email: email,
+              },
             });
             if (error) {
               console.log(error);
@@ -58,12 +81,17 @@ const EditReservationButton = ({ reservation }) => {
         onCancel={() => setModalOpen(false)}
       >
         <div className="flex flex-col">
+          {!reservationValid() && (
+            <Alert message="Please fill in all fields" type="error" showIcon />
+          )}
           <div className="p-4 w-80 flex justify-between">
             <span>Date : </span>
             <DatePicker
               defaultValue={moment(date)}
               format={dateFormat}
-              onChange={(value) => setDate(value.format(dateFormat))}
+              onChange={(value) =>
+                setDate(value ? value.format(dateFormat) : null)
+              }
               className="w-32"
             />
           </div>
@@ -74,7 +102,9 @@ const EditReservationButton = ({ reservation }) => {
               defaultValue={moment(time, timeFormat)}
               className="w-32"
               format={timeFormat}
-              onChange={(value) => setTime(value.format(timeFormat))}
+              onChange={(value) =>
+                setTime(value ? value.format(timeFormat) : null)
+              }
             />
           </div>
           <div className="p-4 w-80 flex justify-between">
@@ -101,15 +131,27 @@ const EditReservationButton = ({ reservation }) => {
           </div>
           <div className="p-4 w-80 flex justify-between">
             <span>Name : </span>
-            <Input style={{ width: "9.1em" }} defaultValue={name} />
+            <Input
+              style={{ width: "9.1em" }}
+              defaultValue={name}
+              onChange={(event) => setName(event.target.value)}
+            />
           </div>
           <div className="p-4 w-80 flex justify-between">
             <span>Last Name : </span>
-            <Input style={{ width: "9.1em" }} defaultValue={lastName} />
+            <Input
+              style={{ width: "9.1em" }}
+              defaultValue={lastName}
+              onChange={(event) => setLastName(event.target.value)}
+            />
           </div>
           <div className="p-4 w-80 flex justify-between">
             <span>Age : </span>
-            <Input style={{ width: "9.1em" }} defaultValue={age} />
+            <Input
+              style={{ width: "9.1em" }}
+              defaultValue={age}
+              onChange={(event) => setAge(event.target.value)}
+            />
           </div>
           <div className="p-4 w-80 flex justify-between">
             <span>Experience : </span>
@@ -125,7 +167,11 @@ const EditReservationButton = ({ reservation }) => {
           </div>
           <div className="p-4 w-80 flex justify-between">
             <span>Email : </span>
-            <Input style={{ width: "9.1em" }} defaultValue={email} />
+            <Input
+              style={{ width: "9.1em" }}
+              defaultValue={email}
+              onChange={(event) => setEmail(event.target.value)}
+            />
           </div>
         </div>
       </Modal>
