6796216e2d953c3093df99b85ea500b4e8bc2050
Adolfo Portilla <aportilla@abnormalsecurity.com>
2022-10-24 11:15:46 -0500

    feat: testing

--- pages/admin.js
+++ pages/admin.js
@@ -5,10 +5,13 @@ import { withPageAuth } from "@supabase/auth-helpers-nextjs";
 // Example: https://github.com/vercel/nextjs-subscription-payments
 // Current Issue: https://github.com/supabase/auth-helpers/issues/281
 
-export default function Admin({ user }) {
+export default function Admin({ user, customProps }) {
   return <Dashboard />;
 }
 
 export const getServerSideProps = withPageAuth({
   redirectTo: "/admin",
+  async getServerSideProps(ctx, supabase) {
+    return { props: { email: "testing" } };
+  },
 });
