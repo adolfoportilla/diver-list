58a0a3370c71f5e21ea120b9168ca1ccd45cf960
Adolfo Portilla <aportilla@abnormalsecurity.com>
2022-10-24 16:55:16 -0500

    Feat: nits

--- pages/admin.js
+++ pages/admin.js
@@ -3,6 +3,6 @@ import Dashboard from "../components/admin_real/shared/Dashboard";
 // Example: https://github.com/vercel/nextjs-subscription-payments
 // Current Issue: https://github.com/supabase/auth-helpers/issues/281
 
-export default function Admin({ user }) {
+export default function Admin() {
   return <Dashboard />;
 }
