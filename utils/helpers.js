088a4747d38366129f62e05cc416b15e9c353cba
Adolfo Portilla <aportilla@abnormalsecurity.com>
2022-10-28 10:29:02 -0500

    Feat: add redirect to url

--- utils/helpers.js
+++ utils/helpers.js
@@ -6,5 +6,6 @@ export const getURL = (path) => {
         process.env.NEXT_PUBLIC_VERCEL_URL !== ""
       ? process.env.NEXT_PUBLIC_VERCEL_URL
       : "http://localhost:3000";
+  console.log("-------------------URL", url);
   return url.includes("http") ? url + path : `https://${url}${path}`;
 };
